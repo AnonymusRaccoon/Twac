aa97147dcccad246a82f9eefd2e07f4ede248de9
Tristan Roux <tristan.roux@epitech.eu>
2019-12-03 18:54:29 +0100

    Adding prefabs

--- src/game_loader.c
+++ src/game_loader.c
@@ -11,9 +11,12 @@
 int start_game(void)
 {
     gc_engine *engine = engine_create("Runner");
-    gc_scene *scene = create_game_scene();
+    gc_scene *scene;
 
-    if (!engine || !scene)
+    if (!engine)
+        return (ERROR);
+    scene = create_game_scene(engine);
+    if (!scene)
         return (ERROR);
     engine->change_scene(engine, scene);
     while (engine->is_open(engine)) {
--- src/scenes/game_scene.c
+++ src/scenes/game_scene.c
@@ -6,17 +6,21 @@
 */
 
 #include "scene.h"
+#include "prefab.h"
 #include <stddef.h>
 
 static const char *textures[] = {"assets/sprites/bck_layer1.png", \
 "assets/sprites/bck_layer2.png", "assets/sprites/bck_layer3.png", \
 "assets/sprites/bck_layer4.png", NULL};
 
-gc_scene *create_game_scene(void)
+gc_scene *create_game_scene(gc_engine *engine)
 {
     gc_scene *scene = scene_create(textures);
+    gc_entity *entity;
 
     if (!scene)
         return (NULL);
+    entity = prefab_load(engine, "prefabs/player.gcprefab");
+    scene->add_entity(scene, entity);
     return (scene);
 }
