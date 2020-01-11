a448374162a714ed93e9afb43c4e740e7a145456
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-01-12 00:10:37 +0100

    Adding a .legend file

--- main.c
+++ main.c
@@ -8,10 +8,12 @@
 #include "runner.h"
 #include "my.h"
 
-int usage(void)
+int usage(const char *bin)
 {
     my_printf("My runner:\n\n\tUsage: %s [path_to_map]. \
-If no map are specified, the default one is loaded.\n");
+If no map are specified, the default one is loaded.\n\n\
+Moves:\n\tQ or D to move to the left or right\n\t\
+SPACE to jump\n", bin);
     return (0);
 }
 
@@ -20,6 +22,6 @@ int main(int argc, char **argv)
     if (argc == 1)
         return (start_game("prefabs/game.gcprefab"));
     if (argc != 2 || !my_strcmp(argv[1], "-h"))
-        return (usage());
+        return (usage(argv[0]));
     return (start_game(argv[1]));
 }
